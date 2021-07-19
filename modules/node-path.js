010af4ae7d60b365b543f9f9937a91f001df0a50
Chris Stead <cmstead@gmail.com>
2021-07-19 11:40:35 -0700

    r Extracted and named location comparisons

--- modules/node-path.js
+++ modules/node-path.js
@@ -1,13 +1,20 @@
 const estraverse = require('estraverse');
 
-function nodeContainsSelection(node, selectionLocation) {
+function startLocationIsOkay(node, selectionLocation) {
     return (node.loc.start.line < selectionLocation.start.line
         || (node.loc.start.line === selectionLocation.start.line
-            && node.loc.start.column <= selectionLocation.start.column))
+            && node.loc.start.column <= selectionLocation.start.column));
+}
 
-        && (node.loc.end.line > selectionLocation.end.line
-            || (node.loc.end.line === selectionLocation.end.line
-                && node.loc.end.column >= selectionLocation.end.column));
+function endLocationIsOkay(node, selectionLocation) {
+    return (node.loc.end.line > selectionLocation.end.line
+        || (node.loc.end.line === selectionLocation.end.line
+            && node.loc.end.column >= selectionLocation.end.column));
+}
+
+function nodeContainsSelection(node, selectionLocation) {
+    return startLocationIsOkay(node, selectionLocation)
+        && endLocationIsOkay(node, selectionLocation);
 }
 
 function buildNodePath(parsedSource, selectionLocation) {
