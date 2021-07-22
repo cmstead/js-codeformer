9b101c71f367f6ca27726c32db1f4fe613882ba1
Chris Stead <cmstead@gmail.com>
2021-07-22 14:12:25 -0700

    c Capturing next thoughts for extract variable

--- modules/commands/extract-variable/extract-variable.js
+++ modules/commands/extract-variable/extract-variable.js
@@ -7,11 +7,19 @@
 // - FunctionExpression
 // - ArrowFunctionExpression (only with FunctionBody)
 
-// 1. [x] get the scope for extraction (window.showQuickPick)
+// 1. [ ] get the scope for extraction (window.showQuickPick)
 // 2. [ ] get the variable name (window.showInputBox)
+// 3. [ ] get variable type (const, let, var -- in that order -- window.showQuickPick)
+
+// 1. [x] build extraction scope path
+// 2. [x] prepare extraction scope data for user scope selection
 // 3. [ ] create variable declaration code
-// 4. [ ] create variable name replacement
-// 5. [ ] select location for variable declaration insertion
+//     1. [ ] capture selected text
+//     2. [ ] build variable source string using
+//              variable type, variable name, and selected source code
+//     3. [ ] Semicolons?? -- I never did figure this out before.
+// 4. [ ] select location for variable declaration insertion
+// 5. [ ] select location for variable name input
 // 6. [ ] create array of edits: [value replacement + location, variable declaration + location]
 
 
