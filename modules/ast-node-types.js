ed5009ac45b0d0016dd78e205212c683735c884d
Chris Stead <cmstead@gmail.com>
2021-07-29 15:08:29 -0700

    R!! Extracted param search logic to new module

--- modules/ast-node-types.js
+++ modules/ast-node-types.js
@@ -1,19 +1,19 @@
 module.exports = {
     ARROW_FUNCTION_EXPRESSION: 'ArrowFunctionExpression',
     BLOCK_STATEMENT: 'BlockStatement',
-    FUNCTION: 'Function',
-    FUNCTION_DECLARATION: 'FunctionDeclaration',
-    FUNCTION_EXPRESSION: 'FunctionExpression',
-    IF_STATEMENT: 'IfStatement',
-    IDENTIFIER: 'Identifier',
-    WHILE_STATEMENT: 'WhileStatement',
     DO_WHILE_STATEMENT: 'DoWhileStatement',
     FOR_STATEMENT: 'ForStatement',
     FOR_IN_STATEMENT: 'ForInStatement',
+    FUNCTION: 'Function',
+    FUNCTION_DECLARATION: 'FunctionDeclaration',
+    FUNCTION_EXPRESSION: 'FunctionExpression',
+    IDENTIFIER: 'Identifier',
+    IF_STATEMENT: 'IfStatement',
     METHOD_DEFINITION: 'MethodDefinition',
     MEMBER_EXPRESSION: 'MemberExpression',
     OBJECT_EXPRESSION: 'ObjectExpression',
     PROGRAM: 'Program',
     VARIABLE_DECLARATION: 'VariableDeclaration',
-    VARIABLE_DECLARATOR: 'VariableDeclarator'
+    VARIABLE_DECLARATOR: 'VariableDeclarator',
+    WHILE_STATEMENT: 'WhileStatement'
 }
--- modules/commands/extract-method/extract-method.js
+++ modules/commands/extract-method/extract-method.js
@@ -1,14 +1,7 @@
-const estraverse = require('estraverse');
-
 const { parse } = require('../../parser/parser');
 const { getSourceSelection } = require('../../source-utilities');
 
-const astNodeTypes = require('../../ast-node-types');
-const {
-    IDENTIFIER,
-    MEMBER_EXPRESSION
-} = astNodeTypes;
-
+const { findAppropriateParameters } = require('./parameter-search');
 
 function parseSelectedText(sourceCodeText, selectionLocation) {
     const sourceSelection = getSourceSelection(sourceCodeText, selectionLocation);
@@ -18,95 +11,6 @@ function parseSelectedText(sourceCodeText, selectionLocation) {
     } catch (_) {
         throw new Error('Selected source cannot be interpreted, unable to extract method');
     }
-}
-
-const scopeTypes = [
-    astNodeTypes.ARROW_FUNCTION_EXPRESSION,
-    astNodeTypes.FUNCTION_DECLARATION,
-    astNodeTypes.FUNCTION_EXPRESSION,
-    astNodeTypes.BLOCK_STATEMENT
-];
-
-const declarationTypes = [
-    astNodeTypes.ARROW_FUNCTION_EXPRESSION,
-    astNodeTypes.FUNCTION_DECLARATION,
-    astNodeTypes.FUNCTION_EXPRESSION,
-    astNodeTypes.VARIABLE_DECLARATOR
-];
-
-function getNodeType(node) {
-    return node !== null ? node.type : '';
-}
-
-function isVariableBeingDeclared(parentNode, node) {
-    const parentNodeType = getNodeType(parentNode);
-
-    return node.type === IDENTIFIER &&
-        (
-            declarationTypes.includes(parentNodeType)
-            || (parentNodeType === MEMBER_EXPRESSION
-                && parentNode.object === node)
-        )
-}
-
-function isVariableBeingUsed(parentNode, node) {
-    const parentNodeType = getNodeType(parentNode);
-    const nodeType = getNodeType(node);
-
-    const parentIsNotAMemberExpression = parentNodeType !== MEMBER_EXPRESSION;
-    const nodeIsAnIdentifier = nodeType === IDENTIFIER;
-
-    return nodeIsAnIdentifier && parentIsNotAMemberExpression;
-}
-
-function isNodeAScope(node) {
-    const nodeType = getNodeType(node);
-
-    return scopeTypes.includes(nodeType);
-}
-
-function insertUsedVariables(parameterNames, variableNamesObject) {
-    parameterNames
-        .forEach(parameterName =>
-            variableNamesObject[parameterName] = true);
-}
-
-function diffVariableSets(declaredVariables, variablesInUse) {
-    return Object.keys(variablesInUse)
-    .filter(function (variableName) {
-        return !declaredVariables[variableName];
-    })
-}
-
-function findAppropriateParameters(parsedSelectionSource) {
-    const declaredVariables = {};
-    const variablesInUse = {};
-
-    estraverse.traverse(parsedSelectionSource, {
-        enter: function (node, parentNode) {
-            if (node === parsedSelectionSource) {
-                return;
-            }
-
-            const varibleIsBeingDeclared = isVariableBeingDeclared(parentNode, node);
-            const variableIsBeingUsed = isVariableBeingUsed(parentNode, node);
-            const nodeIsAScope = isNodeAScope(node);
-
-            if (nodeIsAScope) {
-                const locatedParameters = findAppropriateParameters(node);
-
-                insertUsedVariables(locatedParameters, variablesInUse);
-
-                return estraverse.VisitorOption.Skip;
-            } if (varibleIsBeingDeclared) {
-                declaredVariables[node.name] = true;
-            } else if (variableIsBeingUsed) {
-                variablesInUse[node.name] = true;
-            }
-        }
-    });
-
-    return diffVariableSets(declaredVariables, variablesInUse);
 }
 
 module.exports = {
