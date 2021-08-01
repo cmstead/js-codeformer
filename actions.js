4c6d9dd1fe927791b9d992d18ceff77f9c6f2582
Chris Stead <cmstead@gmail.com>
2021-07-31 23:09:50 -0700

    R!! Stabilized main extension file w config

---
+++ actions.js
@@ -0,0 +1,19 @@
+const actions = [
+	{
+		commandId: 'cmstead.jscodeformer.extractVariable',
+		path: './modules/commands/extract-variable/extract-variable-action',
+		name: 'extractVariable'
+	},
+	{
+		commandId: 'cmstead.jscodeformer.extractMethod',
+		path: './modules/commands/extract-method/extract-method-action',
+		name: 'extractMethod'
+	},
+	{
+		commandId: 'cmstead.jscodeformer.inlineVariable',
+		path: './modules/commands/inline-variable/inline-variable-action',
+		name: 'inlineVariable'
+	}
+];
+
+module.exports = actions;
--- extension.js
+++ extension.js
@@ -1,42 +1,28 @@
 const vscode = require('vscode');
 
-const { extractVariable } = require('./modules/commands/extract-variable/extract-variable-action');
-const { extractMethod } = require('./modules/commands/extract-method/extract-method-action');
-const { inlineVariable } = require('./modules/commands/inline-variable/inline-variable-action');
+const actions = require('./actions');
 
 function activate(context) {
 	const formatDocument = () =>
 		vscode.commands.executeCommand("editor.action.formatDocument")
 
-	let extractVarDisposable = vscode.commands.registerCommand(
-		'cmstead.jscodeformer.extractVariable',
-		function () {
-			extractVariable()
-			.then(formatDocument);
+	actions
+		.forEach(function (action) {
+			const disposable = vscode.commands.registerCommand(
+				action.commandId,
+				function () {
+					const codeAction = require(action.path);
+
+					codeAction[action.name]()
+						.then(formatDocument);
+				}
+			);
+
+			context.subscriptions.push(disposable);
 		});
 
-	context.subscriptions.push(extractVarDisposable);
-
-	let extractMethodDisposable = vscode.commands.registerCommand(
-		'cmstead.jscodeformer.extractMethod',
-		function () {
-			extractMethod()
-			.then(formatDocument);
-		});
-
-	context.subscriptions.push(extractMethodDisposable);
-
-	let inlineVariableDisposable = vscode.commands.registerCommand(
-		'cmstead.jscodeformer.inlineVariable',
-		function () {
-			inlineVariable()
-			.then(formatDocument);
-		});
-
-	context.subscriptions.push(inlineVariableDisposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() { }
 
 module.exports = {
