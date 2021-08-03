6ae309adcc96fb046b2edc5b8f0d0fda7e2b2db3
Chris Stead <cmstead@gmail.com>
2021-08-03 12:04:50 -0700

    R Extracted skip logic for clarity

--- modules/astTraverse.js
+++ modules/astTraverse.js
@@ -1,12 +1,16 @@
 const estraverse = require('estraverse');
 
+function skipTypeScriptSpecificNodes(node) {
+    return node.type.startsWith('TS')
+        ? estraverse.VisitorOption.Skip
+        : node;
+}
+
 function traverse(ast, behaviors) {
     return estraverse.traverse(ast, {
         ...behaviors,
         fallback(node) {
-            return node.type.startsWith('TS')
-                ? estraverse.VisitorOption.Skip
-                : node;
+            return skipTypeScriptSpecificNodes(node);
         }
     });
 }
