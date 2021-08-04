3f19c5485872cc577df3c2d36db1f3e2a88be475
Chris Stead <cmstead@gmail.com>
2021-08-04 10:24:29 -0700

    F Added group information for accessibility

--- actions.js
+++ actions.js
@@ -1,23 +1,33 @@
+const { groups } = require('./groups');
+
 const actions = [
 	{
 		commandId: 'cmstead.jscodeformer.extractVariable',
 		path: './modules/commands/extract-variable/extract-variable-action',
-		name: 'extractVariable'
+		name: 'extractVariable',
+		title: 'Extract Variable',
+		group: groups.REFACTORINGS
 	},
 	{
 		commandId: 'cmstead.jscodeformer.extractMethod',
 		path: './modules/commands/extract-method/extract-method-action',
-		name: 'extractMethod'
+		name: 'extractMethod',
+		title: 'Extract Method',
+		group: groups.REFACTORINGS
 	},
 	{
 		commandId: 'cmstead.jscodeformer.inlineVariable',
 		path: './modules/commands/inline-variable/inline-variable-action',
-		name: 'inlineVariable'
+		name: 'inlineVariable',
+		title: 'Inline Variable',
+		group: groups.REFACTORINGS
 	},
 	{
 		commandId: 'cmstead.jscodeformer.surroundWith',
 		path: './modules/commands/surround-with/surround-with-action',
-		name: 'surroundWith'
+		name: 'surroundWith',
+		title: 'Surround With',
+		group: groups.ACTIONS
 	}
 ];
 
---
+++ groups.js
@@ -0,0 +1,14 @@
+const groups = {
+    REFACTORINGS: 'Refactorings',
+    ACTIONS: 'Actions'
+};
+
+const groupTitles = {
+    [groups.REFACTORINGS]: 'JS CodeFormer Refactorings',
+    [groups.ACTIONS]: 'JS CodeFormer Actions'
+};
+
+module.exports = {
+    groups,
+    groupTitles
+};
