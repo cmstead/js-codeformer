8fedb98e7ea1a0f9e581fdfaf438215a93706e35
Chris Stead <cmstead@gmail.com>
2021-08-04 22:30:36 -0700

    R Removed duplicate logic

--- modules/commands/extract-to-parameter/extract-to-parameter.js
+++ modules/commands/extract-to-parameter/extract-to-parameter.js
@@ -1,6 +1,7 @@
 const { VARIABLE_DECLARATOR, ARROW_FUNCTION_EXPRESSION, FUNCTION_DECLARATION, FUNCTION_EXPRESSION, FUNCTION, METHOD_DEFINITION, VARIABLE_DECLARATION } = require("../../constants/ast-node-types");
 const { findNodeInPath, findNodeByCheckFunction } = require("../../edit-utils/node-path-utils");
-const { getSourceSelection } = require("../../source-utilities");
+
+const { pickVariableDeletionLocation } = require('../../extraction-utils/variable-deletion-utils');
 
 const functionNodeTypes = [
     ARROW_FUNCTION_EXPRESSION,
@@ -29,80 +30,6 @@ function findFunction(nodePath) {
 
 function getFunctionParametersString(functionNode) {
     console.log(functionNode);
-}
-
-function getSourceLines(source, selectedLocation) {
-    const lineStart = selectedLocation.start.line - 1;
-    const lineEnd = selectedLocation.end.line;
-
-    return source
-        .split(/\r?\n/g)
-        .slice(lineStart, lineEnd)
-        .join('\n')
-}
-
-function normalizeSelection(textSelection) {
-    return textSelection.trim().replace(/\s+/g, ' ');
-}
-
-function getNormalizedSourceSelection(source, selectedLocation) {
-    const selection = getSourceSelection(source, selectedLocation);
-
-    return normalizeSelection(selection);
-}
-
-function getNormalizedSourceLines(source, selectedLocation) {
-    const sourceLines = getSourceLines(source, selectedLocation);
-
-    return normalizeSelection(sourceLines);
-}
-
-function buildDeletionLocation(
-    sourceIsDifferentThanLocation,
-    selectedLocation,
-    selectedNode
-) {
-    const {
-        start: { line: startLine, column: startColumn },
-        end: { line: endLine, column: endColumn}
-    } = selectedLocation;
-
-    const selectionIsADeclaration = selectedNode.type === VARIABLE_DECLARATION;
-    const selectionIsADeclarator = selectedNode.type === VARIABLE_DECLARATOR;
-    const onlyDeleteSelection = sourceIsDifferentThanLocation && selectionIsADeclaration;
-
-    if (onlyDeleteSelection) {
-        return selectedLocation;
-    } else if (selectionIsADeclarator) {
-        return {
-            start: { line: startLine, column: startColumn },
-            end: { line: endLine, column: endColumn + 1 }
-        };
-    } else {
-        return {
-            start: { line: startLine, column: 0 },
-            end: { line: endLine + 1, column: 0 }
-        };
-    }
-}
-
-function chooseDeclarationNode(declaratorNode, declarationNode) {
-    const multipleDeclarationsExist = declarationNode.declarations.length > 1;
-    return multipleDeclarationsExist ? declaratorNode : declarationNode
-}
-
-function pickVariableDeletionLocation(declaratorNode, declarationNode, source) {
-    const selectedNode = chooseDeclarationNode(declaratorNode, declarationNode);
-    const selectedLocation = selectedNode.loc;
-
-    const declarationSourceLines = getNormalizedSourceSelection(source, selectedLocation);
-    const rawSourceLines = getNormalizedSourceLines(source, selectedLocation);
-
-    return buildDeletionLocation(
-        rawSourceLines !== declarationSourceLines,
-        selectedLocation,
-        selectedNode
-    );
 }
 
 module.exports = {
