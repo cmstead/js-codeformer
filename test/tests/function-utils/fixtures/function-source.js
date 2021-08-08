58e9db632be1e177d562bd3da7ee6db82f069166
Chris Stead <cmstead@gmail.com>
2021-08-08 15:53:35 -0700

    t Testing function utility behaviors

--- test/tests/function-utils/fixtures/function-source.js
+++ test/tests/function-utils/fixtures/function-source.js
@@ -1,19 +1,19 @@
 class TestClass {
     testMethod () {
-
+        return 'hi';
     }
 }
 
 function namedTestFunction (a, b) {
-
+    return a + b;
 }
 
 const anonymousFunction = function({ x, y, z}) {
-
+    return (x**2 + y**2 + z**2)**0.5;
 }
 
 const arrowFunctionWithBlock = (a, { x }) => {
-
+    return a - x;
 }
 
-const arrowFunctionWithoutBlock = () => null;
+const arrowFunctionWithoutBlock = () => 'foo!';
