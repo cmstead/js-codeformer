f27aca4d8f963363a27fc2c239b23522bae7828e
Chris Stead <cmstead@gmail.com>
2021-08-08 15:33:48 -0700

    F getting name from class method

--- test/tests/function-utils/function-source.test.js
+++ test/tests/function-utils/function-source.test.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { FUNCTION_DECLARATION } = require("../../../modules/constants/ast-node-types");
+const { FUNCTION_DECLARATION, METHOD_DEFINITION } = require("../../../modules/constants/ast-node-types");
 const { getFunctionName } = require("../../../modules/function-utils/function-source");
 const { buildNodePath } = require("../../../modules/node-path");
 const { parse } = require("../../../modules/parser/parser");
@@ -9,7 +9,7 @@ const { readFileSource } = require("../../utilities/file-reader");
 
 describe('function source utils', function () {
     describe('get function name', function () {
-        it('gets name from a function declaration',  function () {
+        it('gets name from a function declaration', function () {
             const sourceCode = readFileSource(__dirname, 'fixtures/function-source.js');
             const ast = parse(sourceCode);
 
@@ -33,5 +33,30 @@ describe('function source utils', function () {
 
             assert.equal(functionName, 'namedTestFunction');
         });
+    });
+
+    it('gets name from a method defintion', function () {
+        const sourceCode = readFileSource(__dirname, 'fixtures/function-source.js');
+        const ast = parse(sourceCode);
+
+        const selection = buildSelectionLocation({
+            start: buildEditorCoordinates({
+                line: 3,
+                column: 1
+            }),
+            end: buildEditorCoordinates({
+                line: 3,
+                column: 1
+            })
+        });
+
+        const selectionPath = buildNodePath(ast, selection);
+        selectionPath.reverse();
+
+        const functionNode = selectionPath.find(node => node.type === METHOD_DEFINITION);
+
+        const functionName = getFunctionName(functionNode);
+
+        assert.equal(functionName, 'testMethod');
     });
 });
