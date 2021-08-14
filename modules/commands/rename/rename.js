6cee64c82b2742d7d3a70ac3284e3a2f47c04f39
Chris Stead <cmstead@gmail.com>
2021-08-13 23:09:36 -0700

    F Updated rename to handle parameters

--- modules/commands/rename/rename.js
+++ modules/commands/rename/rename.js
@@ -1,6 +1,7 @@
-const { VARIABLE_DECLARATOR, FUNCTION_DECLARATION, PROPERTY, FUNCTION_EXPRESSION, METHOD_DEFINITION, BLOCK_STATEMENT, PROGRAM, CLASS_BODY, IDENTIFIER } = require('../../constants/ast-node-types');
+const { VARIABLE_DECLARATOR, FUNCTION_DECLARATION, PROPERTY, FUNCTION_EXPRESSION, METHOD_DEFINITION, BLOCK_STATEMENT, PROGRAM, CLASS_BODY, IDENTIFIER, ARROW_FUNCTION_EXPRESSION } = require('../../constants/ast-node-types');
 const { getNodeType, reverse, last } = require('../../core-utils');
 const { findNodeByCheckFunction } = require('../../edit-utils/node-path-utils');
+const { nodeContainsSelection } = require('../../node-path');
 
 const {
     getVariableDeclaractor,
@@ -19,19 +20,41 @@ function getSurroundingScope(selectionPath) {
 }
 
 const renameableNodeTypes = [IDENTIFIER, VARIABLE_DECLARATOR, FUNCTION_DECLARATION, METHOD_DEFINITION];
+const functionNodeTypes = [
+    FUNCTION_DECLARATION,
+    FUNCTION_EXPRESSION,
+    ARROW_FUNCTION_EXPRESSION
+];
 
 function isNodeAFunctionProperty(node) {
     return getNodeType(node) === PROPERTY
         && getNodeType(node.value) === FUNCTION_EXPRESSION
 }
+
 function isNodeADeclaration(node) {
     return renameableNodeTypes.includes(getNodeType(node))
 }
+
+function isSelectionInParameters(node, location) {
+    let selectionFound = false;
+
+    node.params.forEach(function (parameter) {
+        selectionFound = selectionFound || nodeContainsSelection(parameter, location);
+    });
+
+    return selectionFound;
+}
+
 function isRenameableNode(selectionNode, node) {
     const nodeIsADeclaration = isNodeADeclaration(node);
     const nodeIsAFunctionProperty = isNodeAFunctionProperty(node);
 
-    return nodeIsADeclaration || nodeIsAFunctionProperty;
+    const nodeType = getNodeType(node);
+    const selectionNotInParameters = !functionNodeTypes.includes(nodeType)
+        || node.params.length === 0
+        || !isSelectionInParameters(node, selectionNode.loc) ;
+
+    return selectionNotInParameters && (nodeIsADeclaration || nodeIsAFunctionProperty);
 }
 
 function findDeclaratorOrFunctionDeclaration(selectionPath) {
@@ -45,6 +68,7 @@ function findDeclaratorOrFunctionDeclaration(selectionPath) {
 function isNodeAVariableOrFunctionDeclaration(nodeType) {
     return nodeType !== METHOD_DEFINITION && nodeType !== PROPERTY
 }
+
 function selectReplacementLocations(searchScope, variableDeclarator) {
     const declaratorNodeType = getNodeType(variableDeclarator);
 
@@ -53,9 +77,24 @@ function selectReplacementLocations(searchScope, variableDeclarator) {
         : selectMethodLocations(searchScope, variableDeclarator)
 }
 
+function getVariableDeclaratorLocation(variableDeclarator) {
+    if(typeof variableDeclarator.id === 'object') {
+        return variableDeclarator.id.loc;
+    } else if(typeof variableDeclarator.key === 'object') {
+        return variableDeclarator.key.loc;
+    } else if(typeof variableDeclarator.callee === 'object') {
+        return variableDeclarator.callee.loc;
+    } else if(getNodeType(variableDeclarator) === IDENTIFIER) {
+        return variableDeclarator.loc;
+    }
+
+    throw new Error(`Variable delaration type unknown: ${getNodeType(variableDeclarator)}`)
+}
+
 module.exports = {
     findDeclaratorOrFunctionDeclaration,
     getSurroundingScope,
     getVariableDeclaractor,
+    getVariableDeclaratorLocation,
     selectReplacementLocations
 };
