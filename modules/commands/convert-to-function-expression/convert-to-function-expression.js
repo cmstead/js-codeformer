76b5b68763f265696fef947d059bdf794c1607dc
Chris Stead <cmstead@gmail.com>
2021-08-14 23:44:22 -0700

    B Single-line arrow functions convert properly

--- modules/commands/convert-to-function-expression/convert-to-function-expression.js
+++ modules/commands/convert-to-function-expression/convert-to-function-expression.js
@@ -1,12 +1,13 @@
 const { getNewVariableBuilder, variableTypes } = require("../../builders/VariableBuilder");
 const { getMethodBuilder } = require("../../builders/MethodBuilder");
-const { ARROW_FUNCTION_EXPRESSION, FUNCTION_DECLARATION, FUNCTION_EXPRESSION, FUNCTION } = require("../../constants/ast-node-types");
+const { ARROW_FUNCTION_EXPRESSION, FUNCTION_DECLARATION, FUNCTION_EXPRESSION, FUNCTION, BLOCK_STATEMENT } = require("../../constants/ast-node-types");
 
 const {
     getFunctionBody,
     getFunctionName,
     getFunctionParametersString
 } = require('../../function-utils/function-source');
+const { getNodeType } = require("../../core-utils");
 
 const functionNodeTypes = [
     ARROW_FUNCTION_EXPRESSION,
@@ -15,10 +16,24 @@ const functionNodeTypes = [
     FUNCTION
 ];
 
+function isSingleLineArrowFunction(functionNode) {
+    return getNodeType(functionNode) === ARROW_FUNCTION_EXPRESSION
+        && getNodeType(functionNode.body) !== BLOCK_STATEMENT
+}
+
+function buildFunctionBody(functionNode, sourceText) {
+    const functionBody = getFunctionBody(functionNode, sourceText);
+
+    return isSingleLineArrowFunction(functionNode)
+        ? `return ${functionBody}`
+        : functionBody;
+
+}
+
 function getNewFunctionString(functionNode, sourceText) {
     const functionName = getFunctionName(functionNode);
     const functionParameters = getFunctionParametersString(functionNode, sourceText);
-    const functionBody = getFunctionBody(functionNode, sourceText);
+    const functionBody = buildFunctionBody(functionNode, sourceText);
 
     const functionString = getMethodBuilder({
         functionParameters: functionParameters,
